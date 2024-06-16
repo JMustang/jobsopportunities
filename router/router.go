40ce51f451e8946c2597c35023d2c4885817845e
Junior <eecfredes@gmail.com>
2024-06-16 00:34:37 -0300

    :rocket: adicionando novas rotas

--- router/router.go
+++ router/router.go
@@ -1,13 +1,14 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Initialize() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default() // <-Inicializa a ROTA
+
+	// As ROTAS seram inicializadas aqui
+	initializeRoutes(r)
+
+	r.Run(":8080") // Roda o servifor
 }
