dab144174186d044d11ad9de9f59d44be0f50a48
Junior <eecfredes@gmail.com>
2024-06-19 22:04:56 -0300

    :rocket: Config | Database

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/JMustang/jobsopportunities/router"
+import (
+	"fmt"
+
+	"github.com/JMustang/jobsopportunities/config"
+	"github.com/JMustang/jobsopportunities/router"
+)
 
 func main() {
+
+	// Inicializando Config
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	router.Initialize()
 }
