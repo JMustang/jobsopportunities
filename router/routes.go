14c9dd8fdf1098ca75a46f664ebc703a56d8a2f6
Junior <eecfredes@gmail.com>
2024-06-27 21:33:07 -0300

    :rocket: configurando o swagger

--- router/routes.go
+++ router/routes.go
@@ -1,15 +1,23 @@
 package router
 
 import (
+	docs "github.com/JMustang/jobsopportunities/docs"
 	"github.com/JMustang/jobsopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(r *gin.Engine) {
 
 	// Iniciando o handler
 	handler.InitializeHandler()
-	v1 := r.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+
+	// Definindo as rotas
+
+	v1 := r.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -17,4 +25,6 @@ func initializeRoutes(r *gin.Engine) {
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
 	}
+	// Inicializando o Swagger
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
